9b591ea1f26c1085d0e8f5ac420f6fc006e0326c
Shezad Ahamed <shezadahamed95@gmail.com>
2017-08-22 14:35:02 +0530

    Example and  Screenshots updated

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Focaloid Technologies on 22/08/17.
+//  Copyright © 2017 Focaloid Technologies. All rights reserved.
+//
+
+import UIKit
+import SHTransition
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
